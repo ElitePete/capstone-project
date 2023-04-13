09152df09f3fefb56bb3762f85ee0310d10d7133
ElitePete <105971332+ElitePete@users.noreply.github.com>
2023-04-13 04:11:46 -0400

    added addToCart feature

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,16 +1,20 @@
 import './cart-item.styles.scss'
 
-const CartItem = () => {
-    const { name } = ({ cartItem }) => {
-        const { name, quantity } = cartItem;
-    }
+const CartItem = ({ cartItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    
     return (
-        <div>
-            <h2>{name}</h2>
+        <div className='cart-item-container'>
+            <img src={imageUrl} alt={`${name}`} />
+            <div className='item-details'>
+                <span className='name'>{name}</span>
+                <span className='price'>{quantity} x ${price}</span>
+
+            </div>
 
         </div>
     )
-}
+};
 
 
 export default CartItem;
